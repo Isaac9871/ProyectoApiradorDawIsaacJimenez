1fe822a191c6f2daf7f6045940f7d7cb6e99d122
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-11-05 17:45:00 +0100

    Crea menú del programa, con sus respectivas variables y bucles para que se repita

--- src/main/java/proyectoaspiradordawisaac/Aspirador.java
+++ src/main/java/proyectoaspiradordawisaac/Aspirador.java
@@ -5,10 +5,40 @@
  */
 package proyectoaspiradordawisaac;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author isaac
  */
 public class Aspirador {
-    
+
+    public static void main(String[] args) {
+//Declarar variables
+//Variables INT
+        int opcion;
+//Variables boolean
+        boolean repetir = true;
+//Blucle Do Wile, que se repite  todo el programa o finaliza el programa si el valor repetir es false
+        do {
+//Bucle Do While,Que se repite si introduces un valor que no esté dentro del menú
+            do {
+//Menú donde eligiremos la opción que deseamos realizar 
+                String opcionString = JOptionPane.showInputDialog(null, "Seleccione la opción deseada:\n"
+                        + "1-Configurar el Sistema\n"
+                        + "2-Carga\n"
+                        + "3-Aspiración\n"
+                        + "4-Aspiración y fregado\n"
+                        + "5-Estado general\n"
+                        + "6-Base de carga\n"
+                        + "0-Salir\n");
+
+//Convertir el valor String anterior a un valor Int
+                opcion = Integer.parseInt(opcionString);
+            } while (opcion < 0 || opcion > 6);
+
+        } while (repetir);
+//Mensaje que se muestra al finalizar el programa
+        JOptionPane.showMessageDialog(null, "Saliendo del programa....");
+    }
 }
