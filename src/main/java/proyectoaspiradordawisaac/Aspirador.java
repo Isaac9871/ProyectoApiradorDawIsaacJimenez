d7f6cd07cd1b9152a4c8b9ef778e14a64f1cce29
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-11-06 18:27:03 +0100

    Crea el case 2 y sus variables, este case se encarga de asignar el % de bateria de la aspiradora

--- src/main/java/proyectoaspiradordawisaac/Aspirador.java
+++ src/main/java/proyectoaspiradordawisaac/Aspirador.java
@@ -24,7 +24,7 @@ public class Aspirador {
         boolean repetir = true;
 
         //Variables Double
-        double metros, total, banio = 0, salon = 0, cocina = 0, dormi = 0, dormi2 = 0;
+        double metros, total, carga, banio = 0, salon = 0, cocina = 0, dormi = 0, dormi2 = 0;
 
 //Variables String
         String texto = "";
@@ -54,7 +54,7 @@ public class Aspirador {
                 //Te reidirije a la opcion uno
                 opcion = 1;
 
-            } 
+            }
 //Switch para realizar las acciones del menú anterior
             switch (opcion) {
 //Case 1 es el de configurar el Sistema, donde estableceremos los metros de las habitaciones
@@ -123,8 +123,21 @@ public class Aspirador {
 
                     break;
 
+//Case 2 es el que se encarga de asignar el % de bateria en ese momento de la aspiradora
                 case 2:
+//Bucle que limita el valor que podemos introducir entre 0 y 100
+                    do {
+//Introducir la cantidad de carga que tiene el robot(String)
+                        String cargaString = JOptionPane.showInputDialog(null, "Cuanto % de batería tiene el robot actualmente:");
+//Convertir el valor String al valor Double
+                        carga = Double.parseDouble(cargaString);
+//If que muestra un mensaje cuando se introduce un valor que no está permitido
+                        if (carga < 0 || carga > 100) {
+                            JOptionPane.showMessageDialog(null, "Valor no valido..");
+                        }
+                    } while (carga < 0 || carga > 100);
                     break;
+
                 case 3:
                     break;
 
